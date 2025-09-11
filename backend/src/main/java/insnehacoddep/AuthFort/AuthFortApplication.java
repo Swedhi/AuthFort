c0a57a0ea920e49d34edf6661a788e15d33b33c5
Swedhi <snehapal153@gmail.com>
2025-09-11 22:52:21 +0530

    added java backend code

---
+++ backend/src/main/java/insnehacoddep/AuthFort/AuthFortApplication.java
@@ -0,0 +1,13 @@
+package insnehacoddep.AuthFort;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthFortApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthFortApplication.class, args);
+	}
+
+}
